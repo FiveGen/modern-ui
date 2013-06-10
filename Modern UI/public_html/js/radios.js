b775cca455513e1a0f4c23db2ca113c678317882
vbsessa <vicente.bissoli@gmail.com>
2013-06-10 16:51:16 -0300

    .

---
+++ Modern UI/public_html/js/radios.js
@@ -0,0 +1,57 @@
+/* 
+ *  Universidade Federal do Espírito Santo
+ *  Núcleo de Processamento de Dados
+ *  Todos os direitos reservados
+ 
+ *  Autor: Vicente Bissoli Sessa
+ *  Contato: vicente.sessa@ufes.br
+ */
+
+
+var radio = {
+    config: {
+        selector: 'input[type=radio]',
+        wrapperClass: 'radio-wrapper',
+        wrapperElem: '<span>',
+        wrapperSelector: '.radio-wrapper',
+        virtualRadioClass: 'radio',
+        virtualRadioElem: '<span>',
+        virtualRadioSelector: '.radio-wrapper .radio'
+    },
+    init: function() {
+        this.wrap()
+            .hover()
+            .click();
+    },
+    wrap: function() {
+        $(this.config.selector).each(function() {
+            $(this).after(radio.config.virtualRadioElem).next().addClass(radio.config.virtualRadioClass);
+            $(this).nextUntil(radio.config.selector).andSelf()
+                .wrapAll(radio.config.wrapperElem).parent().addClass(radio.config.wrapperClass);
+        });
+        return this;
+    },
+    hover: function() {
+        $(this.config.wrapperSelector).hover(function() {
+            $(this).children(radio.config.virtaulRadioSelector).addClass('hover');
+        }, function() {
+            $(this).children(radio.config.virtaulRadioSelector).removeClass('hover');
+        });
+        return this;
+    },
+    click: function() {
+        $(this.config.wrapperSelector).on('click', function() {
+            $(this).siblings(radio.config.wrapperSelector)
+                .children(radio.config.virtualRadioSelector).removeClass('checked')
+                .end().children(radio.config.selector).prop('checked', false);
+
+            $(this).children(radio.config.virtualRadioSelector).addClass('checked')
+                .end().children(radio.config.selector).prop('checked', true);
+        });
+        return this;
+    }
+};
+
+jQuery(function($) {
+    radio.init();
+});
