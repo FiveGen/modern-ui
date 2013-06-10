9e23adfb0b3910150a80bba9691f68e8783bb56e
vbsessa <vicente.bissoli@gmail.com>
2013-06-10 17:05:45 -0300

    .

---
+++ Modern UI/public_html/js/checkbox.js
@@ -0,0 +1,12 @@
+/* 
+ *  Universidade Federal do Espírito Santo
+ *  Núcleo de Processamento de Dados
+ *  Todos os direitos reservados
+ 
+ *  Autor: Vicente Bissoli Sessa
+ *  Contato: vicente.sessa@ufes.br
+ */
+
+
+var checkbox = {
+};
